e89e6c231ba50125ab1b76597af5c8eaae7e3833
klequis <klequis@gmail.com>
2018-01-05 17:07:35 -0800

    initial

---
+++ trial01/trial01.js
@@ -0,0 +1,19 @@
+function sum1(a, b) {
+  // return the sum of the parameters
+  return a + b
+}
+
+function sum2(fox, charlie, tango) {
+  // return the sum of the parameters
+  return fox + charlie + tango
+}
+
+function multiply1(num1, num2) {
+  // multiply each parameter by 10 and return the sum of both products
+  return num1 * 10 + num2* 10
+
+}
+
+
+
+module.exports = { sum1, sum2, multiply1 }
---
+++ trial01/trial01.test.js
@@ -0,0 +1,13 @@
+const { sum1, sum2, multiply1 } = require('./sum');
+
+test('sum1: adds 1 + 2 to equal 3', () => {
+  expect(sum1(1, 2)).toBe(3);
+})
+
+test('sum2: adds 1 + 2 + 3 to equal 6', () => {
+  expect(sum2(1, 2, 3)).toBe(6);
+})
+
+test('multiply1: 2 * 10 + 3 * 10 = 50', () => {
+  expect(multiply1(2, 3)).toBe(50);
+})
